af3776ca4f9f6d315cf0d0fcfd649a8c45197e3b
jtas575 <jtas575@aucklanduni.ac.nz>
2026-05-27 16:51:34 +1200

    basic Person View and changed Navbar links to Person

--- TechnicalAssessment/Program.cs
+++ TechnicalAssessment/Program.cs
@@ -22,7 +22,7 @@ app.MapStaticAssets();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}")
+    pattern: "{controller=Person}/{action=Index}/{id?}")
     .WithStaticAssets();
 
 

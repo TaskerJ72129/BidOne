de2110d9ec7c75653a6100bfbb4ec4514a26dc51
jtas575 <jtas575@aucklanduni.ac.nz>
2026-05-27 16:18:00 +1200

    Initial MVC setup

---
+++ TechnicalAssessment/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace TechnicalAssessment.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}

f2c0925df37ef3046c8e1153ac7f15f0b32b2194
jtas575 <jtas575@aucklanduni.ac.nz>
2026-05-27 17:57:42 +1200

    Added POST logic and json file for the data

--- TechnicalAssessment/Controllers/PersonController.cs
+++ TechnicalAssessment/Controllers/PersonController.cs
@@ -1,10 +1,13 @@
 using Microsoft.AspNetCore.Mvc;
 using TechnicalAssessment.Models;
+using System.Text.Json;
 
 namespace TechnicalAssessment.Controllers;
 
 public class PersonController : Controller
 {   
+    private static readonly string FilePath = "data.json";
+
     [HttpGet]
     public IActionResult Index()
     {
@@ -14,7 +17,26 @@ public class PersonController : Controller
     [HttpPost]
     public IActionResult Index(Person person)
     {
-        return View();
-    }
+        var people = new List<Person>();
 
+        if (System.IO.File.Exists(FilePath))
+        {
+            var currentJson = System.IO.File.ReadAllText(FilePath);
+
+            if (!string.IsNullOrWhiteSpace(currentJson))
+            {
+                people = JsonSerializer.Deserialize<List<Person>>(currentJson) ?? new List<Person>();
+            }
+        }
+
+        people.Add(person);
+
+        var updatedJson = JsonSerializer.Serialize(people, new JsonSerializerOptions{
+            WriteIndented = true
+        });
+
+        System.IO.File.WriteAllText(FilePath, updatedJson);
+
+        return RedirectToAction("Index");
+    }
 }
